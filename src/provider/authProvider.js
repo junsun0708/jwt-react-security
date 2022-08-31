79b9d582a2d6c1b9d5f41575f847848915b4e100
JYH <salvayh@thingspire.com>
2022-08-31 15:18:47 +0900

    1. cors 오류 - proxy 설정 2. 로그인 메서드 연동 완료

--- src/provider/authProvider.js
+++ src/provider/authProvider.js
@@ -2,11 +2,29 @@ import axios from "axios";
 
 const authProvider = {
   login: ({ username, password }) => {
-    if (username !== "john" || password !== "123") {
-      return Promise.reject();
-    }
-    localStorage.setItem("username", username);
-    return Promise.resolve();
+    const request = new Request("http://localhost:8080/auth/login", {
+      method: "POST",
+      body: JSON.stringify({ email: username, password: password }),
+      headers: new Headers({ "Content-Type": "application/json" }),
+    });
+    return fetch(request)
+      .then((response) => {
+        if (response.status < 200 || response.status >= 300) {
+          throw new Error(response.statusText);
+        }
+        console.log("1111", response.status, response.json());
+        //return response.json();
+        localStorage.setItem("username", username);
+        return Promise.resolve();
+      })
+      .then((auth) => {
+        localStorage.setItem("username", username);
+        localStorage.setItem("auth", JSON.stringify(auth));
+      })
+      .catch(() => {
+        console.log("3333");
+        throw new Error("Network error");
+      });
   },
   logout: () => {
     localStorage.removeItem("username");
